4d74273f7b90eca0cfb5ac0a91c02b475e3c7f93
Adrian Lorenčič <adrianlorencic98@gmail.com>
2022-09-09 13:31:51 +0200

    Imported saving system

---
+++ Assets/SavingWrapper.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+public class SavingWrapper : MonoBehaviour
+{
+    const string defaultSaveFile = "save";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Save();
+        }
+    }
+
+    private void Load()
+    {
+        GetComponent<SavingSystem>().Load(defaultSaveFile);
+    }
+
+    private void Save()
+    {
+        GetComponent<SavingSystem>().Save(defaultSaveFile);
+    }
+}
